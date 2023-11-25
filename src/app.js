c75453c82a9baf015c68b47fe446ed61ac9e061e
Luong Duong <luongduongit99@gmail.com>
2023-11-25 21:36:31 +0700

    Necessary folders and packages when creating a Project!

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const {
+    default: helmet
+} = require('helmet')
+const compression = require('compression')
+const app = express();
+
+// init middleware
+app.use(morgan('dev'));
+// app.use(morgan('combined'));
+app.use(helmet())
+// app.use(compression());
+
+// init db 
+
+// init routes
+app.get('/', (req, res, next) => {
+    const strCompress = 'Duongh399'
+    return res.status(200).json({
+
+        message: 'Wellcome Nodejs server',
+        metadata: strCompress.repeat(1000)
+    })
+})
+// handling error 
+
+module.exports = app
